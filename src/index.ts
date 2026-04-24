2c05d0ca9e0955e8d3580e9955e4c3c78aef04b8
Younes Lahouiti <loller763@gmail.com>
2026-04-24 23:51:12 +0200

    creation of the smil façade obj

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-export {};
+import { SMILTween } from "./core/SMILTween.ts";
+import type { TweenTarget, TweenVars } from "./types/index.ts";
+
+export const smil = {
+  to: (targets: TweenTarget, vars: TweenVars): SMILTween =>
+    new SMILTween(targets, vars),
+
+  from: (targets: TweenTarget, vars: TweenVars): SMILTween =>
+    new SMILTween(targets, vars, null, true),
+
+  fromTo: (targets: TweenTarget, fromVars: TweenVars, toVars: TweenVars): SMILTween =>
+    new SMILTween(targets, toVars, fromVars),
+
+  set: (targets: TweenTarget, vars: TweenVars): SMILTween =>
+    new SMILTween(targets, { ...vars, duration: 0, ease: "none" }),
+};
+
+// Public API re-exports
+export { SMILTween } from "./core/SMILTween.ts";
+export { Animation } from "./core/Animation.ts";
+export type {
+  TweenVars,
+  TweenTarget,
+  StaggerObject,
+  EaseString,
+  PositionParam,
+} from "./types/index.ts";
