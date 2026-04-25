5dfdef7208345886905afdd384fe174196b59e4f
Younes Lahouiti <loller763@gmail.com>
2026-04-25 15:03:18 +0200

    rename to prepare

--- tests/unit/utils/builders.ts.test.ts
+++ tests/unit/utils/builders.ts.test.ts
@@ -8,68 +8,68 @@ describe("builders", () => {
   describe("buildAnimate", () => {
     // ? one describe per function in the file
     // * Test paths, HAPPY PATHs and EDGE CASEs, (order is important, fist HP then EC)
-    it.todo("HAPPY PATH: should route properties", () => {
+    it.todo("HAPPY PATH: should do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("HAPPY PATH: should route properties 2", () => {
+    it.todo("HAPPY PATH: should do stuff 2", () => {
       expect(1).toBe(1);
     });
 
-    it.todo("EDGE CASE: shouldn't route properties", () => {
+    it.todo("EDGE CASE: shouldn't do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("EDGE CASE: shouldn't route properties 2", () => {
+    it.todo("EDGE CASE: shouldn't do stuff 2", () => {
       expect(1).toBe(1);
     });
   });
   describe("buildAnimateTransform", () => {
     // ? one describe per function in the file
     // * Test paths, HAPPY PATHs and EDGE CASEs, (order is important, fist HP then EC)
-    it.todo("HAPPY PATH: should route properties", () => {
+    it.todo("HAPPY PATH: should do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("HAPPY PATH: should route properties 2", () => {
+    it.todo("HAPPY PATH: should do stuff 2", () => {
       expect(1).toBe(1);
     });
 
-    it.todo("EDGE CASE: shouldn't route properties", () => {
+    it.todo("EDGE CASE: shouldn't do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("EDGE CASE: shouldn't route properties 2", () => {
+    it.todo("EDGE CASE: shouldn't do stuff 2", () => {
       expect(1).toBe(1);
     });
   });
   describe("buildSet", () => {
     // ? one describe per function in the file
     // * Test paths, HAPPY PATHs and EDGE CASEs, (order is important, fist HP then EC)
-    it.todo("HAPPY PATH: should route properties", () => {
+    it.todo("HAPPY PATH: should do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("HAPPY PATH: should route properties 2", () => {
+    it.todo("HAPPY PATH: should do stuff 2", () => {
       expect(1).toBe(1);
     });
 
-    it.todo("EDGE CASE: shouldn't route properties", () => {
+    it.todo("EDGE CASE: shouldn't do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("EDGE CASE: shouldn't route properties 2", () => {
+    it.todo("EDGE CASE: shouldn't do stuff 2", () => {
       expect(1).toBe(1);
     });
   });
   describe("injectInto", () => {
     // ? one describe per function in the file
     // * Test paths, HAPPY PATHs and EDGE CASEs, (order is important, fist HP then EC)
-    it.todo("HAPPY PATH: should route properties", () => {
+    it.todo("HAPPY PATH: should do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("HAPPY PATH: should route properties 2", () => {
+    it.todo("HAPPY PATH: should do stuff 2", () => {
       expect(1).toBe(1);
     });
 
-    it.todo("EDGE CASE: shouldn't route properties", () => {
+    it.todo("EDGE CASE: shouldn't do stuff", () => {
       expect(1).toBe(1);
     });
-    it.todo("EDGE CASE: shouldn't route properties 2", () => {
+    it.todo("EDGE CASE: shouldn't do stuff 2", () => {
       expect(1).toBe(1);
     });
   });
