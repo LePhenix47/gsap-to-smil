78c4e8898a5887d7ab005e314ca2e384066fe038
Younes Lahouiti <loller763@gmail.com>
2026-05-08 18:51:07 +0200

    smil default export

--- src/index.ts
+++ src/index.ts
@@ -2,22 +2,26 @@ import { SMILTween } from "./core/SMILTween.ts";
 import { SMILTimeline } from "./core/SMILTimeline.ts";
 import type { TweenTarget, TweenVars, TimelineVars } from "./types/index.ts";
 
-export const smil = {
+const smil = {
   to: (targets: TweenTarget, vars: TweenVars): SMILTween =>
     new SMILTween(targets, vars),
 
   from: (targets: TweenTarget, vars: TweenVars): SMILTween =>
     new SMILTween(targets, vars, null, true),
 
-  fromTo: (targets: TweenTarget, fromVars: TweenVars, toVars: TweenVars): SMILTween =>
-    new SMILTween(targets, toVars, fromVars),
+  fromTo: (
+    targets: TweenTarget,
+    fromVars: TweenVars,
+    toVars: TweenVars,
+  ): SMILTween => new SMILTween(targets, toVars, fromVars),
 
   set: (targets: TweenTarget, vars: TweenVars): SMILTween =>
     new SMILTween(targets, { ...vars, duration: 0, ease: "none" }),
 
-  timeline: (vars?: TimelineVars): SMILTimeline =>
-    new SMILTimeline(vars),
+  timeline: (vars?: TimelineVars): SMILTimeline => new SMILTimeline(vars),
 };
+
+export default smil;
 
 // Public API re-exports
 export { AnimationDebugger } from "./utils/animation-debugger.ts";
